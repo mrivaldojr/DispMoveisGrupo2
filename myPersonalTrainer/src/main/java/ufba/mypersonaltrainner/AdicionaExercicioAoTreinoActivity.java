da66f784c9eb3aa937d88159c64ac35725423aea
Lain Carvalho <lain.cesar@gmail.com>
2014-11-17 09:36:40 -0300

    Adicionei popup de criar exercicio no novo treino.

---
+++ myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/AdicionaExercicioAoTreinoActivity.java
@@ -0,0 +1,60 @@
+package ufba.mypersonaltrainner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class AdicionaExercicioAoTreinoActivity extends Activity {
+
+    //public static String EXERCICIO
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adiciona_exercicio_ao_treino);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_adiciona_exercicio_ao_treino, menu);
+        return true;
+    }
+
+    public void confirma(View view) {
+        Intent i = new Intent();
+
+        EditText nome = (EditText) findViewById(R.id.add_exercicio_nome_do_exercicio);
+        EditText series = (EditText) findViewById(R.id.add_exercicio_series_do_exercicio);
+        EditText carga = (EditText) findViewById(R.id.add_exercicio_carga_do_exercicio);
+
+        i.putExtra(ConfigurarTreinoActivity.CHAVE_NOME, nome.getText().toString());
+        i.putExtra(ConfigurarTreinoActivity.CHAVE_SERIES, series.getText().toString());
+        i.putExtra(ConfigurarTreinoActivity.CHAVE_CARGA, carga.getText().toString());
+
+        setResult(Activity.RESULT_OK, i);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/ConfigurarTreinoActivity.java
+++ myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/ConfigurarTreinoActivity.java
@@ -1,19 +1,68 @@
 package ufba.mypersonaltrainner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ConfigurarTreinoActivity extends Activity {
+
+    private ListView listViewExercicios;
+    private ArrayAdapter<String> adapterExercicios;
+    static final int CRIA_EXERCICIO_REQUEST = 0;
+    static final String CHAVE_NOME = "chave";
+    static final String CHAVE_SERIES = "series";
+    static final String CHAVE_CARGA = "nome";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_treino_my);
+
+        List<String> lst = new ArrayList<String>();
+
+        adapterExercicios = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, lst);
+
+        listViewExercicios =(ListView) findViewById(R.id.list_execiciosAdicionados);
+        listViewExercicios.setAdapter(adapterExercicios);
+
+        listViewExercicios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getBaseContext(), TrainigDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    public void addExercicioDialog(View view) {
+        // Do something in response to button click
+        Intent i = new Intent(this,AdicionaExercicioAoTreinoActivity.class);
+        startActivityForResult(i, CRIA_EXERCICIO_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == CRIA_EXERCICIO_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String nome = data.getStringExtra(CHAVE_NOME);
+                String series = data.getStringExtra(CHAVE_SERIES);
+                String carga = data.getStringExtra(CHAVE_CARGA);
+                adapterExercicios.add(nome + " " + series + " " + carga);
+                adapterExercicios.notifyDataSetChanged();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
