57d61b12778c8bf027b0a489e64fb9a1f2db137b
Lain Carvalho <lain.cesar@gmail.com>
2014-11-24 03:01:26 -0300

    pra poder pull

---
+++ myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/util/PK.java
@@ -0,0 +1,10 @@
+package ufba.mypersonaltrainner.util;
+
+/**
+ * Created by usuario on 11/24/2014.
+ */
+public class PK {
+    public static final String TIPOS_DE_EXERCICIO = "EXE_exercicio";
+    public static final String TREINOS = "TRN_treino";
+    public static final String EXERCICIOS = "exercicio";
+}
