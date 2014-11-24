d1f4bea626ce5b59fc19bda5b9e01612b8b4f7b3
junior <mrivaldojr@gmail.com>
2014-11-24 00:48:57 -0300

    Melhorias de interface

--- myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/TrainingDetail.java
+++ myPersonalTrainer/src/main/java/ufba/mypersonaltrainner/TrainingDetail.java
@@ -80,7 +80,7 @@ public class TrainingDetail extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_edit_train) {
             return true;
         }
         return super.onOptionsItemSelected(item);
