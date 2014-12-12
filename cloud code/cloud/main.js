16d2b8b15863118eebee8c83129114e66c2d1392
Lain Carvalho <lain.cesar@gmail.com>
2014-12-11 23:34:36 -0300

    Tratamento de push e  Parse.Cloud.job("aviso_treino_diario")

--- cloud code/cloud/main.js
+++ cloud code/cloud/main.js
@@ -2,5 +2,21 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
+});
+
+Parse.Cloud.job("aviso_treino_diario", function(request, status) {
+
+	Parse.Push.send({
+		channels: [""],
+		data: { alert: "Seu treino Hoje é..." }
+	}, {
+	success: function() {
+		status.success("Alerta enviado com sucesso.");
+	},
+	error: function(error) {
+		console.log(error);
+		status.error("Algo deu errado.");
+	}
+	});
 });
